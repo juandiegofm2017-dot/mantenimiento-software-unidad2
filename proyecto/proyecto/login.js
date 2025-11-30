bfe0a6698d138d1a91d160db413515b4d07dd4a5
juandiegofm2017-dot <juandiegofm2017@gmail.com>
2025-11-29 23:38:04 -0500

    Update login.js

--- proyecto/proyecto/login.js
+++ proyecto/proyecto/login.js
@@ -1,3 +1,4 @@
+// Actualización realizada por Gabriel
 function registrar(){
     let usuario = document.getElementById("usuario").value;
     document.getElementById("msg").innerHTML = "Usuario registrado: " + usuario;
