e558051cd40eb564b0d2d1daa84b59469272bf2f
juandiegofm2017-dot <juandiegofm2017@gmail.com>
2025-12-13 20:57:39 -0500

    Create storage.js

---
+++ proyecto/js/core/storage.js
@@ -0,0 +1,19 @@
+// core/storage.js
+(function () {
+  const USERS_KEY = "ms_users";
+
+  function getUsers() {
+    const raw = localStorage.getItem(USERS_KEY);
+    try {
+      return raw ? JSON.parse(raw) : [];
+    } catch (e) {
+      return [];
+    }
+  }
+
+  function saveUsers(users) {
+    localStorage.setItem(USERS_KEY, JSON.stringify(users));
+  }
+
+  window.StorageMS = { getUsers, saveUsers, USERS_KEY };
+})();
