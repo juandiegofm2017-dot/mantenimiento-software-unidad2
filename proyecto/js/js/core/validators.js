5072c08fffe3e6ae8a40e95dfed605b7325c7984
juandiegofm2017-dot <juandiegofm2017@gmail.com>
2025-12-13 20:11:05 -0500

    Create validators.js

---
+++ proyecto/js/js/core/validators.js
@@ -0,0 +1,11 @@
+(function () {
+  function required(value) {
+    return value !== null && value !== undefined && String(value).trim().length > 0;
+  }
+
+  function minLength(value, n) {
+    return String(value).trim().length >= n;
+  }
+
+  window.ValidatorsMS = { required, minLength };
+})();
